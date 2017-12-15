debf7e6b1914a9da402697ebc73f632e291184d5
Jay Martinez <jay.martinez7@yahoo.com>
2017-12-14 19:57:00 -0600

    react activities

---
+++ 03-Stu_HelloDiv/src/components/HelloDiv.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HelloDiv = () => (
+	<div>
+		<h1>Hey</h1>
+		<h2>Things I like!</h2>
+		<ul>
+			<li>Soccer</li>
+			<li>Gym</li>
+		</ul>
+	</div>	
+
+	);
+
+export default HelloDiv;
